7461e785a4a39dec7896d26efce546c1cd9e6dc6
nurmuh-alhakim18 <alhakim2318@gmail.com>
2025-02-08 14:00:14 +0700

    create dependency injection

--- cmd/http.go
+++ cmd/http.go
@@ -4,16 +4,29 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/nurmuh-alhakim18/evermos-project/helpers"
 	"github.com/nurmuh-alhakim18/evermos-project/internal/handlers"
+	"github.com/nurmuh-alhakim18/evermos-project/internal/interfaces"
 	"github.com/nurmuh-alhakim18/evermos-project/internal/services"
 )
 
 func ServeHTTP() {
 	app := fiber.New()
+	dependency := dependencyInject()
 
-	healthSvc := &services.HealthService{}
-	healthHandler := &handlers.HealthHandler{Service: healthSvc}
-	app.Get("/health", healthHandler.HealthCheck)
+	app.Get("/health", dependency.healthHandler.HealthCheck)
 
 	port := helpers.GetEnv("PORT", "8080")
 	app.Listen(":" + port)
+}
+
+type Dependency struct {
+	healthHandler interfaces.HealthHandlerInterface
+}
+
+func dependencyInject() Dependency {
+	healthSvc := &services.HealthService{}
+	healthHandler := &handlers.HealthHandler{Service: healthSvc}
+
+	return Dependency{
+		healthHandler: healthHandler,
+	}
 }
